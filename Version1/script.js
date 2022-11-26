f6eee83272a674614ff2553ba18198bd1bec4ff8
Philip Pretorius <philippretorius.79@gmail.com>
2022-11-26 18:56:58 +0200

    Finished

--- Version1/script.js
+++ Version1/script.js
@@ -5,10 +5,40 @@ const userChoiceDisplay = document.getElementById('user-choice');
 const resultDisplay = document.getElementById('result');
 const possibleChoices = document.querySelectorAll('button');
 let userChoice;
+let computerChoice;
+let result;
 
 possibleChoices.forEach(possibleChoices => possibleChoices.addEventListener('click', (e) => {
     userChoice = e.target.id;
     userChoiceDisplay.innerHTML = userChoice;
+    generateComputerChoice();
+    getResult();
 }))
 
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3) + 1; 
+    // or you can use possibleChoices.length
+    
+    if (randomNumber === 1) {
+        computerChoice = 'Rock';
+    } else if (randomNumber === 2) {
+        computerChoice = 'Paper';      
+    } else if (randomNumber === 3) {
+        computerChoice = 'Scissors';
+    }
 
+    computerChoiceDisplay.innerHTML = computerChoice;
+} 
+
+function getResult() {
+    if (computerChoice === userChoice) {
+        result = 'It\'s a Draw!';
+    } 
+    if (computerChoice === 'Rock' && userChoice === 'Scissors' || computerChoice === 'Paper' && userChoice === 'Rock' || computerChoice === 'Scissors' && userChoice === 'Paper') {
+        result = 'You Lose!';
+    } 
+    if (computerChoice === 'Rock' && userChoice === 'Paper' || computerChoice === 'Paper' && userChoice === 'Scissors' || computerChoice === 'Scissors' && userChoice === 'Rock') {
+        result = 'You Win!';
+    }
+    resultDisplay.innerHTML = result;
+} 

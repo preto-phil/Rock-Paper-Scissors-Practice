d2492283464f1d3cfe998133f7bebb0815646b17
Philip Pretorius <philippretorius.79@gmail.com>
2022-12-06 23:44:21 +0200

    Sesh end

--- Practice/UIscript.js
+++ Practice/UIscript.js
@@ -1,6 +1,118 @@
 
+//////////////
 
-let computerSelection;
+//// create variable linked to button
+
+const rockButton = document.querySelector('#Rock');
+
+const paperButton = document.querySelector('#Paper');
+
+const scissorButton = document.querySelector('#Scissors');
+
+
+//// Create event listeners added to buttons
+
+rockButton.addEventListener('click', playRound());
+
+paperButton.addEventListener('click', playRound());
+
+scissorsButton.addEventListener('click', playRound());
+
+
+//// Player Selection
+
+playerSelection = 
+
+/////////////////////
+
+
+
+
+function getComputerSelection() {  
+    let myArray = ['Rock', 'Paper', 'Scissors'];
+    let randomArray = Math.floor(Math.random * myArray.length);
+    let computerSelection = myArray[randomArray];
+    return computerSelection;
+} 
+
+function playRound() {
+    
+    getComputerSelection();
+    
+    if ( playerSelection === 'ROCK' && computerSelection === 'PAPER' || playerSelection === 'PAPER' && computerSelection === 'SCISSORS' || playerSelection === 'SCISSORS' && computerSelection === 'ROCK' ) {
+        computerScore = computerScore + 1;
+        return "You lose the round!";
+    }
+    
+    if (playerSelection === 'ROCK' && computerSelection === 'SCISSORS' || playerSelection === 'PAPER' && computerSelection === 'ROCK' || playerSelection === 'SCISSORS' && computerSelection === 'PAPER' ) {
+        playerScore = playerScore + 1;
+        return "You win the round!";
+    }
+    
+    if (playerSelection === 'ROCK' && computerSelection === 'ROCK' || playerSelection === 'PAPER' && computerSelection === 'PAPER' || playerSelection === 'SCISSORS' && computerSelection === 'SCISSORS' ) {
+        return "It's a draw!";
+    }
+    console.log(getComputerSelection())
+    console.log();
+}
+
+
+
+
+
+
+
+
+
+
+//// Console log when buttons clicked
+
+console.log();
+
+console.log();
+
+console.log();
+
+console.log();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+
+
+
 let playerSelection;
 let round = 0;
 let computerScore = 0;
@@ -9,14 +121,10 @@ let playerScore = 0;
 
 let computerSelectionDisplay = document.getElementById("computerSelectionDisplay");
 
-// Generate computer choice function
-
 function getComputerSelection() {
     
-    // generate random number
     let randomNumber = Math.floor(Math.random() * 3);
 
-    // allocate random number to specific choice of RPS
     if (randomNumber === 0) {
         computerSelection = 'Rock';
     }
@@ -29,86 +137,4 @@ function getComputerSelection() {
     computerSelectionDisplay.innerHTML = computerSelection;
 } 
 
-
-
-
-function playRound() {
-    
-    getComputerSelection();
-    
-    if ( playerSelection === 'ROCK' && computerSelection === 'PAPER' || playerSelection === 'PAPER' && computerSelection === 'SCISSORS' || playerSelection === 'SCISSORS' && computerSelection === 'ROCK' ) {
-        computerScore = computerScore + 1;
-        return "You lose the round!";
-    }
-    
-    if (playerSelection === 'ROCK' && computerSelection === 'SCISSORS' || playerSelection === 'PAPER' && computerSelection === 'ROCK' || playerSelection === 'SCISSORS' && computerSelection === 'PAPER' ) {
-        playerScore = playerScore + 1;
-        return "You win the round!";
-    }
-    
-    if (playerSelection === 'ROCK' && computerSelection === 'ROCK' || playerSelection === 'PAPER' && computerSelection === 'PAPER' || playerSelection === 'SCISSORS' && computerSelection === 'SCISSORS' ) {
-        return "It's a draw!";
-    }
-
-}
-
-
-
-function getPlayerSelection() {
-
-
-
-}
-
-
-function playRound() {
-    
-    getComputerSelection();
-    
-    if ( playerSelection === 'ROCK' && computerSelection === 'PAPER' || playerSelection === 'PAPER' && computerSelection === 'SCISSORS' || playerSelection === 'SCISSORS' && computerSelection === 'ROCK' ) {
-        computerScore = computerScore + 1;
-        return "You lose the round!";
-    }
-    
-    if (playerSelection === 'ROCK' && computerSelection === 'SCISSORS' || playerSelection === 'PAPER' && computerSelection === 'ROCK' || playerSelection === 'SCISSORS' && computerSelection === 'PAPER' ) {
-        playerScore = playerScore + 1;
-        return "You win the round!";
-    }
-    
-    if (playerSelection === 'ROCK' && computerSelection === 'ROCK' || playerSelection === 'PAPER' && computerSelection === 'PAPER' || playerSelection === 'SCISSORS' && computerSelection === 'SCISSORS' ) {
-        return "It's a draw!";
-    }
-
-}
-
-
-
-function game() {
-
-    for (i = 0; i < ; i++) {
-        
-        round = round + 1;
-        console.log("Round: " + round);
-        
-        selection();
-
-        console.log("Player Choice: " + playerSelection);
-               
-        console.log(playRound());   
-              
-        console.log("Player Score: " + playerScore);
-        
-        console.log("Computer Score: " + computerScore);
-        
-    }
-    if (computerScore > playerScore ) {
-        console.log("You lose the game!")
-    } else if ( computerScore < playerScore ) {
-        console.log("You win the game!")
-    } else {
-        console.log("You've drawn the game!")
-    }
-}
-
-console.log(game());
-
+*/
